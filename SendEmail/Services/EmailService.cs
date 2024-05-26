d5c0def6539baf43c9a2260db422a56511e9559b
Cam1101 <jcparradoc@javeriana.edu.co>
2024-05-25 23:28:18 -0500

    Agregar archivos de proyecto.

---
+++ SendEmail/Services/EmailService.cs
@@ -0,0 +1,42 @@
+﻿using SendEmail.Models;
+using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace SendEmail.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void SenqEmail(EmailDTO request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_config.GetSection("Email:UserName").Value));
+            email.To.Add(MailboxAddress.Parse(request.Para));
+            email.Subject = request.Asunto;
+            email.Body = new TextPart(TextFormat.Html)
+            {
+                Text = request.Contenido
+            };
+
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(
+                _config.GetSection("Email:Host").Value,
+                Convert.ToInt32(_config.GetSection("Email:Port").Value),
+                SecureSocketOptions.StartTls);
+
+            smtp.Authenticate(_config.GetSection("Email:Username").Value,
+                _config.GetSection("Email:PassWord").Value);
+
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
